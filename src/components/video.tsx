e978257f220872f4c5793e3c3f368e6baa85d1ca
Jaayden Halko <jaayden.halko@gmail.com>
2024-04-11 21:26:07 -0500

    add skeleton for video component

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,40 +1,50 @@
 "use client";
 
 import React from "react";
+import { Skeleton } from "@nextui-org/react";
 
 interface VideoProps {
   fileName: string;
   videoRef: (element: HTMLVideoElement) => void;
 }
 
-const onCanPlay = (fileName: string) => {
-  console.log(`Video onCanPlay ${fileName}`);
-};
-
 const Video: React.FC<VideoProps> = ({ fileName, videoRef }) => {
+  const [isLoaded, setIsLoaded] = React.useState(false);
   // const { blobs } = await list({
   //   prefix: fileName,
   //   limit: 1,
   // });
   // const { url } = blobs[0];
 
+  const onCanPlay = (fileName: string) => {
+    console.log(`Video loaded data ${fileName}`);
+  };
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 750);
+  });
+
   return (
-    <video
-      ref={videoRef}
-      className="scale-150"
-      width="120"
-      height="160"
-      loop
-      muted
-      controls
-      preload="auto"
-      playsInline
-      aria-label="Video player"
-      onCanPlay={() => onCanPlay(fileName)}
-    >
-      <source src={fileName} type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
+    <Skeleton isLoaded={isLoaded} className="rounded-lg">
+      <video
+        ref={videoRef}
+        className="scale-150"
+        width="120"
+        height="160"
+        loop
+        muted
+        controls
+        preload="auto"
+        playsInline
+        aria-label="Video player"
+        onCanPlay={() => onCanPlay(fileName)}
+      >
+        <source src={fileName} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </Skeleton>
   );
 };
 
