3a88d56fbb2b3c6b217d74f93517a1287855f2a0
Jaayden Halko <jaayden.halko@gmail.com>
2024-04-19 12:04:46 -0500

    add more exercise videos

--- src/app/_analytics/providers.tsx
+++ src/app/_analytics/providers.tsx
@@ -1,13 +1,48 @@
-// app/providers.js
 "use client";
+
+import { type Session } from "next-auth";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
+import { getServerAuthSession } from "~/server/auth";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   });
 }
-export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+export async function CSPostHogProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerAuthSession();
+  return (
+    <PostHogProvider client={posthog}>
+      {/* <PostHogAuthWrapper session={session}>{children}</PostHogAuthWrapper> */}
+      {children}
+    </PostHogProvider>
+  );
 }
+
+// function PostHogAuthWrapper({
+//   children,
+//   session,
+// }: {
+//   children: React.ReactNode;
+//   session?: Session | null;
+// }) {
+//   useEffect(() => {
+//     if (session?.user) {
+//       posthog.identify(session.user.id, {
+//         email: session.user.email,
+//         name: session.user.name,
+//       });
+//     } else if (!session?.user) {
+//       posthog.reset();
+//     }
+//   }, [session]);
+
+//   return children;
+// }
+//
